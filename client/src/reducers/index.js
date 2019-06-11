f56d79994258745cced67a50080e6facf740745d
jose marte <tumusicalatina@gmail.com>
2019-06-11 11:38:44 -0400

    Redux sucks

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,15 @@
 import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import errorReducer from "./errorReducer";
+import authReducer from "./authReducers";
+import errorReducer from "./errorReducers";
+
+/*
+const rootReducer = combineReducers({
+  auth: authReducer,
+  errors: errorReducer
+});
+
+export default rootReducer;
+*/
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer
